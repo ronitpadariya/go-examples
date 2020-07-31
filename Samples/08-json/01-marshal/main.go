78983d46f0d4b1fda1ad5642a63422dbf8a28a12
Ronit <rntpadariya38@gmail.com>
2020-07-31 09:50:34 +0530

    Json has been started

---
+++ Samples/08-json/01-marshal/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+func main() {
+	p1 := person{
+		First: "James",
+		Last:  "Bond",
+		Age:   34,
+	}
+
+	p2 := person{
+		First: "Miss",
+		Last:  "MoneyPenny",
+		Age:   32,
+	}
+
+	people := []person{p1, p2}
+
+	fmt.Println(people)
+
+	bs, err := json.Marshal(people)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(bs))
+}

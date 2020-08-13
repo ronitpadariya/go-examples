0807e6507405cbbd6d4ef7465bbf258cbd91e109
Ronit <rntpadariya38@gmail.com>
2020-08-13 08:44:18 +0530

    Ninja Level 8 completed

---
+++ NinjaExcercizes/NinjaLevel8/exe01/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User struct {
+	First string
+	Age   int
+}
+
+func main() {
+	u1 := User{
+		First: "James",
+		Age:   32,
+	}
+
+	u2 := User{
+		First: "Moneypenny",
+		Age:   27,
+	}
+
+	u3 := User{
+		First: "M",
+		Age:   54,
+	}
+
+	users := []User{u1, u2, u3}
+
+	fmt.Println(users)
+
+	bs, error := json.Marshal(users)
+	if error != nil {
+		fmt.Println(error)
+	}
+	fmt.Println(string(bs))
+}
